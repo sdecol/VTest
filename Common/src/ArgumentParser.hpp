0d730b8f9b49f4a2706a891bdbe0ec4c862f596e
tvpaint <tvpaint@localhost.localdomain>
2024-11-25 16:29:28 +0100

    Added a class to parse arguments

---
+++ Common/src/ArgumentParser.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <string>
+#include <vector>
+
+namespace nCommon
+{
+
+class ArgumentParser
+{
+
+public:
+
+    enum ArgumentType
+    {
+        Number,
+        Range,
+        String
+    };
+
+    // Construction / Destruction
+
+    explicit ArgumentParser( const std::string & iID, ArgumentType iType, const std::string& iDefaultValue);
+    ~ArgumentParser() = default;
+
+    // Arguments validation
+    inline void AddValidArguments(const std::vector<std::string>& iArguments) noexcept { mValidArguments.insert(mValidArguments.end(), iArguments.begin(), iArguments.end());}
+
+    //Checks if the given argument is a valid one for this parser
+    [[nodiscard]]
+    inline bool IsValidArgument(const std::string& iArgument) const noexcept { return std::find(mValidArguments.begin(), mValidArguments.end(), iArgument) != mValidArguments.end();}
+
+    //Checks if the given value is valid and assign it to mValue if so
+    void ParseValue(const std::string& iValue);
+
+    //Asserts that this argument can be converted into a int and returns the converted value
+    [[nodiscard]]
+    inline int ToInt() const { assert(mType == Number); return mValue.empty() ? std::stoi(mDefaultValue) : std::stoi(mValue); }
+
+private:
+
+    std::string mID;
+    ArgumentType mType;
+    std::vector<std::string> mValidArguments;
+    std::string mValue;
+    std::string mDefaultValue;
+
+};
+
+} //nCommon
--- Server/src/Main/main.cpp
+++ Server/src/Main/main.cpp
@@ -1,4 +1,50 @@
+#include <complex>
+
+#include "ArgumentParser.hpp"
+
+#include <iostream>
+#include <vector>
+
 int main(int argc, char** argv)
 {
+    std::vector<nCommon::ArgumentParser*> parsers;
+
+    nCommon::ArgumentParser portParser("port", nCommon::ArgumentParser::Number, "4242");
+    portParser.AddValidArguments({"-p", "--port"});
+    parsers.push_back(&portParser);
+
+    nCommon::ArgumentParser limitParser("limit", nCommon::ArgumentParser::Number, "-1");
+    limitParser.AddValidArguments({"-l", "--limit"});
+    parsers.push_back(&limitParser);
+
+    nCommon::ArgumentParser boundsParser("bounds", nCommon::ArgumentParser::Range, "1,100");
+    boundsParser.AddValidArguments({"-b", "--bounds"});
+    parsers.push_back(&boundsParser);
+
+    std::string argument;
+
+    //We start bind checking arguments received
+    for(int i = 1;i < argc; i+=2) {
+
+        argument = argv[i];
+
+        bool argumentFound = false;
+
+        for(auto* parser : parsers)
+        {
+            if(parser->IsValidArgument(argument)) {
+
+                if(i < argc-1)
+                    parser->ParseValue(argv[i+1]);
+
+                argumentFound = true;
+
+            }
+        }
+
+        if(!argumentFound)
+            std::cout<<"Invalid argument: "<<argument<<std::endl;
+    }
+
     return 0;
 }
